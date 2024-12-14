3c164fb6be98355dede57cc9e34dc2f4cc5c4418
Mumtaz203 <mitaserdogan@gmail.com>
2024-12-14 21:14:07 +0300

    Modified SubscriptedCustomerService.java

---
+++ src/main/java/com/backend/application/services/SubscriptedCustomerService.java
@@ -0,0 +1,31 @@
+package com.backend.application.services;
+
+import com.backend.adapters.in.rest.dto.SubscriptedCustomerDTO;
+import com.backend.adapters.in.rest.mapper.SubscriptedCustomerMapper;
+import com.backend.domain.entities.SubscriptedCustomer;
+
+import com.backend.domain.repository.ISubscriptionCustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubscriptedCustomerService extends BaseService<SubscriptedCustomer , SubscriptedCustomerDTO , Long> {
+
+    @Autowired
+    private SubscriptedCustomerMapper mapper;
+    @Autowired
+    private ISubscriptionCustomerRepository repository;
+
+
+    public SubscriptedCustomerService(ISubscriptionCustomerRepository repository, SubscriptedCustomerMapper mapper) {
+        super(repository, mapper);
+    }
+
+    public SubscriptedCustomerDTO createSubscribedCustomer(SubscriptedCustomerDTO subscriptedCustomerDTO) {
+        SubscriptedCustomer sCustomer = mapper.toEntity(subscriptedCustomerDTO);
+        SubscriptedCustomer savedEntity = repository.save(sCustomer);
+        return mapper.toDto(savedEntity);
+    }
+
+
+}
