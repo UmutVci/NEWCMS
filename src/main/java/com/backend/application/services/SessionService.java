735b819dd1cb35b5f7ef7f5524c28ad02704ec2a
Mumtaz203 <mitaserdogan@gmail.com>
2024-12-14 17:39:43 +0300

    Modified SessionService.java

--- src/main/java/com/backend/application/services/SessionService.java
+++ src/main/java/com/backend/application/services/SessionService.java
@@ -1,17 +1,17 @@
 package com.backend.application.services;
 
 import com.backend.adapters.in.rest.dto.SessionDTO;
-import com.backend.adapters.in.rest.mapper.BaseMapper;
+
 import com.backend.adapters.in.rest.mapper.SessionMapper;
-import com.backend.domain.entities.MovieEntity;
+
 import com.backend.domain.entities.SessionEntity;
-import com.backend.domain.repository.BaseRepository;
+
 import com.backend.domain.repository.ISessionRepository;
 import com.backend.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.management.RuntimeMBeanException;
+
 import java.time.LocalDateTime;
 
 
@@ -24,26 +24,23 @@ public class SessionService extends BaseService<SessionEntity, SessionDTO , Long
     private SessionMapper sessionMapper;
 
 
-
     public SessionService(ISessionRepository sessionRepository, SessionMapper mapper) {
         super(sessionRepository, mapper);
     }
 
-    public SessionDTO createSession(SessionDTO sessionDTO){
-SessionEntity sessionEntity =sessionMapper.toEntity(sessionDTO);
-SessionEntity savedSession=sessionRepository.save(sessionEntity);
-return sessionMapper.toDto(savedSession);
+    public SessionDTO createSession(SessionDTO sessionDTO) {
+        SessionEntity sessionEntity = sessionMapper.toEntity(sessionDTO);
+        SessionEntity savedSession = sessionRepository.save(sessionEntity);
+        return sessionMapper.toDto(savedSession);
     }
 
 
-public LocalDateTime endTimeBySessionId(Long sessionId){
-        SessionEntity session =sessionRepository.findById(sessionId).orElseThrow(()->
+    public LocalDateTime endTimeBySessionId(Long sessionId) {
+        SessionEntity session = sessionRepository.findById(sessionId).orElseThrow(() ->
                 new ResourceNotFoundException("Session not found with ID" + sessionId));
-        int movieDuration=session.getMovie().getDuration();
+        int movieDuration = session.getMovie().getDuration();
         return session.getStartTime().plusMinutes(movieDuration);
-}
-
-
+    }
 
 
 }
