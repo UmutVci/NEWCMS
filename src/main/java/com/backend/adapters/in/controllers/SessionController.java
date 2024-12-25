f170165448a943eb8b928fd9388dc2dc1bd19b2f
Mumtaz203 <mitaserdogan@gmail.com>
2024-12-25 18:23:43 +0300

    Modified SessionController

---
+++ src/main/java/com/backend/adapters/in/controllers/SessionController.java
@@ -0,0 +1,33 @@
+package com.backend.adapters.in.controllers;
+
+import com.backend.adapters.in.rest.dto.SessionDTO;
+import com.backend.application.services.BaseService;
+import com.backend.application.services.SessionService;
+import com.backend.domain.entities.SessionEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/api/session")
+
+
+public class SessionController extends BaseController<SessionEntity, SessionDTO,Long  > {
+    private SessionService sessionService;
+
+    public SessionController(BaseService<SessionEntity, SessionDTO, Long> service) {
+        super(service);
+    }
+
+    @GetMapping("/{id}/session/end")
+    public ResponseEntity<LocalDateTime> endTimeBySessionId(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(sessionService.endTimeBySessionId(id));
+    }
+
+
+}
