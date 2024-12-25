5de4f334a8c24b94581644bbcaeb52aad7cf4553
Mumtaz203 <mitaserdogan@gmail.com>
2024-12-25 18:24:11 +0300

    Modified SubscriptedCustomerController

---
+++ src/main/java/com/backend/adapters/in/controllers/SubscriptedCustomerController.java
@@ -0,0 +1,19 @@
+package com.backend.adapters.in.controllers;
+
+import com.backend.adapters.in.rest.dto.SubscriptedCustomerDTO;
+import com.backend.application.services.BaseService;
+import com.backend.domain.entities.SubscriptedCustomer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/session")
+
+public class SubscriptedCustomerController extends BaseController<SubscriptedCustomer, SubscriptedCustomerDTO , Long> {
+
+
+    public SubscriptedCustomerController(BaseService<SubscriptedCustomer, SubscriptedCustomerDTO, Long> service) {
+        super(service);
+    }
+
+}
